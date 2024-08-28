77bdcd6f331ef7c99630e4852d24857234a2b5a1
Edson Espinoza <edesro1996@gmail.com>
2024-08-28 00:11:05 -0500

    Ejercicios JS agregados

---
+++ exercises.js
@@ -0,0 +1,14 @@
+//Ejercicio 1
+let valor=20, porcentaje=50;
+console.log(`El valor de porcentaje es: ${(porcentaje/100) * valor}`);
+
+//Ejercicio 2
+let numero=10;
+numero % 2 == 0 ? console.log('Número par') : console.log('Numero impar');
+
+//Ejercicio 3
+let contador = 0;
+while(contador < 10){
+    console.log(`El valor actual es ${contador}`);
+    contador ++;
+}
